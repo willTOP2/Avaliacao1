404d21b7907baaeb9bae6e37205f6015a5672f38
Willian <willian.rodrygues146@gmail.com>
2021-07-31 12:06:36 -0300

    TDD - 1
    Testa Falhando

---
+++ test/processBoleto.test.js
@@ -0,0 +1,66 @@
+
+
+
+describe('Processador de boleto', () => {
+
+
+
+  const fatura = {
+    nome: "Jose da Silva", 
+    data: "28/09/2020", 
+    valorTotal: 11300.00
+  }
+  
+
+     const boleto = [{
+
+     codigo: 3459, 
+     data: "23/04/2009", 
+     valorPago: 1300.00
+
+     },
+
+       {
+         codigo: 234, 
+         data: "25/06/2020", 
+         valorPago: 100.00
+       },
+       {
+       codigo: 345, 
+       data: "23/04/2010", 
+       valorPago: 100.00
+       },
+       {
+       codigo: 34, 
+       data: "23/04/2011", 
+       valorPago: 1200.00
+       }
+
+    ]
+
+    const pagamento =  {
+
+   
+      valorPago: 1300.00,
+      data: "12/04/2021",
+      tipoPagamento: "boleto"
+     
+  
+      }
+      
+
+    const qtd = sistema.QuantidadeBoleto(boleto) 
+  
+    
+
+
+    })
+
+      
+   
+  
+
+
+
+
+
